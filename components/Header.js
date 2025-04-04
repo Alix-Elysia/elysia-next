80c4810397a2c516e6ad3c9de47967775f887d0a
alix <aghersallah@gmail.com>
2025-04-04 09:55:23 +0200

    ✨ Nouveau design pour le Header

--- components/Header.js
+++ components/Header.js
@@ -3,52 +3,44 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="bg-[#efede8] shadow-lg p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        {/* Logo */}
+    <header className="bg-[#efede8] py-4 shadow-sm sticky top-0 z-50">
+      <div className="flex flex-col items-center">
+        {/* Logo centré */}
         <Link href="/">
-          <a className="flex items-center">
-            <Image src="/logo-elysia.png" alt="Élysia Logo" width={150} height={50} />
+          <a className="mb-2 opacity-90 hover:opacity-100 transition-opacity duration-200">
+            <Image
+              src="/logo-elysia.png"
+              alt="Logo Elysia"
+              width={140}
+              height={50}
+              priority
+            />
           </a>
         </Link>
 
-        {/* Menu de navigation */}
+        {/* Navigation élégante */}
         <nav>
-          <ul className="flex space-x-6">
+          <ul className="flex space-x-5 text-sm font-light tracking-wide text-[#6f442e] uppercase">
             <li>
-              <Link href="/">
-                <a className="text-[#6f442e] hover:text-[#221f20]">Accueil</a>
-              </Link>
+              <Link href="/"><a className="hover:underline hover:underline-offset-4">Accueil</a></Link>
             </li>
             <li>
-              <Link href="/a-propos">
-                <a className="text-[#6f442e] hover:text-[#221f20]">À propos</a>
-              </Link>
+              <Link href="/a-propos"><a className="hover:underline hover:underline-offset-4">À propos</a></Link>
             </li>
             <li>
-              <Link href="/sanctuaire">
-                <a className="text-[#6f442e] hover:text-[#221f20]">Sanctuaire</a>
-              </Link>
+              <Link href="/sanctuaire"><a className="hover:underline hover:underline-offset-4">Sanctuaire</a></Link>
             </li>
             <li>
-              <Link href="/inscription">
-                <a className="text-[#6f442e] hover:text-[#221f20]">Inscription</a>
-              </Link>
+              <Link href="/gardien"><a className="hover:underline hover:underline-offset-4">IA Le Gardien</a></Link>
             </li>
             <li>
-              <Link href="/gardien">
-                <a className="text-[#6f442e] hover:text-[#221f20]">IA Le gardien</a>
-              </Link>
+              <Link href="/inscription"><a className="hover:underline hover:underline-offset-4">Inscription</a></Link>
             </li>
             <li>
-              <Link href="/contact">
-                <a className="text-[#6f442e] hover:text-[#221f20]">Contact</a>
-              </Link>
+              <Link href="/contact"><a className="hover:underline hover:underline-offset-4">Contact</a></Link>
             </li>
             <li>
-              <Link href="/login">
-                <a className="text-[#6f442e] hover:text-[#221f20]">Se connecter</a>
-              </Link>
+              <Link href="/login"><a className="hover:underline hover:underline-offset-4">Connexion</a></Link>
             </li>
           </ul>
         </nav>
